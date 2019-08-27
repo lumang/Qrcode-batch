880c7f4ac3c1505a7c8b41992ba37357e347f556
lumang <luguannan0107@126.com>
2019-08-27 16:06:47 +0800

    add basic ui

--- README.md
+++ README.md
@@ -3,12 +3,21 @@
 # 需要的包 requirement package
 > pip install qrcode
 # 参考 ref
-[qrcode 文档](https://pypi.org/project/qrcode/)
+[qrcode](https://pypi.org/project/qrcode/)
+
+[tkinter](https://www.runoob.com/python/python-gui-tkinter.html) 
 # TODO
 + [x] 批量生成
-+ []  basic ui
++ [x]  basic ui
 + [] win桌面版
 + [] mac桌面版
-# 更新
+# 更新 update
+## 2019-8-27
+basic ui done 
+1.choose input file (.csv or .txt)
+2.choose output directory
+3.click button Qrcode Batch 
+4.wait a second 
+![basic](./20190827.png)
 ## 2019-8-26
 完成基础功能，导入文件，批量生成二维码到指定文件
--- qrcode-batch.py
+++ qrcode-batch.py
@@ -1,5 +1,9 @@
 import qrcode
 import os 
+from tkinter import *
+from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askopenfilename
+
 
 qr = qrcode.QRCode(
     version=1,
@@ -9,20 +13,51 @@ qr = qrcode.QRCode(
     
 )
 
-file = open('D:\\qrcode\\qrcode.csv')
-readlines = file.readlines()
-i=0
-for readline in readlines:
-    
-    qr.add_data(readline.strip())
-    qr.make(fit=True)
-    img = qr.make_image()
-    filename='D:\\qrcode\\'+readline.strip()+'.png'
-    readline=''
-    img.save(filename)
-    qr.clear()
-    i=i+1
-    print('done '+str(i))
-print('sucess '+str(i))
+#choose output directory
+def selectPath():
+    path_ = askdirectory()
+    print(path_)
+    pathdir.set(path_)
+#choose input file
+def selectFile():
+    path_ = askopenfilename()
+    print(path_)
+    pathfile.set(path_)
+#QrCode Batch
+def QrCodeBatch(pathfile,pathdir):
+    file = open(pathfile)
+    readlines = file.readlines()
+    i=0
+    for readline in readlines:
+        
+        qr.add_data(readline.strip())
+        qr.make(fit=True)
+        img = qr.make_image()
+        filename=pathdir+'.png'
+        readline=''
+        img.save(filename)
+        qr.clear()
+        i=i+1
+        print('done No. '+str(i))
+    print('sucess '+str(i))
+    file.close()
 
-file.close()
+root = Tk()
+root.title("QrCode Batch")
+pathfile = StringVar()
+pathdir = StringVar()
+Label(root,text = "input file:").grid(row = 0, column = 0)
+Entry(root, textvariable = pathfile).grid(row = 0, column = 1)
+Button(root, text = "input file", command = selectFile).grid(row = 0, column = 2)
+
+Label(root,text = "output dir:").grid(row = 1, column = 0)
+Entry(root, textvariable = pathdir).grid(row = 1, column = 1)
+Button(root, text = "outputdir", command = selectPath).grid(row = 1, column = 2)
+
+Button(root,text="QrCode Batch",command=QrCodeBatch).grid(row=2,column = 1)
+
+
+
+
+
+root.mainloop()
